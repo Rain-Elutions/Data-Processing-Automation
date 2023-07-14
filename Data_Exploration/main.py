5059ad1c1312a5e608620b24961965db59b51607
Zhuoyu Feng <zfeng0401@elutions.com>
2023-07-14 11:11:38 -0400

    add annotation to the Data Exploration class

--- Data_Exploration/main.py
+++ Data_Exploration/main.py
@@ -8,26 +8,49 @@ class DataExploration:
         self.data = None
     
     def load_data(self, file_path):
+        """
+        Load data into memory
+        
+        Parameters:
+        - file_path: the path of the data file
+        """
         if file_path.endswith('.csv'):
             self.data = pd.read_csv(file_path)
         elif file_path.endswith('.xlsx'):
             self.data = pd.read_excel(file_path)
     
     def get_data_size(self):
+        """
+        Get the size of the data
+        
+        Returns:
+        - self.data.shape: the size of the data
+        """
         if self.data is not None:
             return self.data.shape
         else:
             return "No data loaded"
     
     def get_data_type(self):
+        """
+        Return the data types and the number of each type
+        
+        Returns:
+        - the number of each data type
+        """
         if self.data is not None:
             # return self.data.dtypes
-            # get the number of each data type
             return self.data.dtypes.value_counts()
         else:
             return "No data loaded"
     
     def summarize_missing_data(self):
+        """
+        Summarize the number of missing data
+
+        Returns:
+        - the number of missing data for columns with missing data
+        """
         if self.data is not None:
             missing_data = self.data.isnull().sum()
             return missing_data[missing_data > 0]
@@ -35,6 +58,12 @@ class DataExploration:
             return "No data loaded"
     
     def visualize_missing_data(self):
+        """
+        Visualize the missing data
+        
+        Returns:
+        - the missing data plots
+        """
         if self.data is not None:
             if self.data.shape[1] > 200:
                 print("Too many features to visualize")
