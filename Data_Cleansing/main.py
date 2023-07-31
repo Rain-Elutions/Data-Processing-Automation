5db599cf42223670d6cc864f44e8c2c64efbe4c8
Zhuoyu Feng <zfeng0401@elutions.com>
2023-07-31 15:24:38 -0400

    intial implement of detect_outliers function

--- Data_Cleansing/main.py
+++ Data_Cleansing/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 from abc import ABC, abstractmethod
+import sys
+sys.path.append('../')
+from Data_Visualization.eda import EDA_Visualization
 
 class FillMissingStrategy(ABC):
     @abstractmethod
@@ -36,7 +39,7 @@ class NotFill(FillMissingStrategy):
 
 
 class DataCleansing:
-    def __init__(self, data):
+    def __init__(self, data: pd.DataFrame = None):
         self.data = data
         # self.fill_missing_strategy = fill_missing_strategy
     
@@ -51,6 +54,7 @@ class DataCleansing:
         - data: the data after removing duplicates
         '''
 
+        data = data if data is not None else self.data
         # drop duplicate rows
         data = data.drop_duplicates()
         # drop columns with same column names
@@ -59,7 +63,7 @@ class DataCleansing:
         return data
 
     
-    def handle_missing_values(self, data: pd.DataFrame, target_list: list = [], drop_threshold: float = 0.5, fill_missing_strategy=FillMissingStrategy) -> pd.DataFrame:
+    def handle_missing_values(self, data: pd.DataFrame = None, target_list: list = [], drop_threshold: float = 0.5, fill_missing_strategy=FillMissingStrategy) -> pd.DataFrame:
         '''
         Handle missing values in the data
 
@@ -72,7 +76,8 @@ class DataCleansing:
         Returns:
         - data: the data after handling missing values
         '''
-         
+        
+        data = data if data is not None else self.data
         # drop rows with missing values in the target_list
         data = data.dropna(subset=target_list)
 
@@ -96,9 +101,29 @@ class DataCleansing:
         #     print("No data loaded")
         pass
     
-    def remove_outliers(self, column):
-        if self.data is not None:
-            z_scores = np.abs(stats.zscore(self.data[column]))
-            self.data = self.data[np.abs(z_scores) <= 3]
-        else:
-            print("No data loaded")
+    def detect_outliers(self, data: pd.DataFrame = None, col_name: str = None , threshold : float = 3) -> pd.DataFrame:
+        '''
+        Detect and plot outliers for a column in the data
+
+        Parameters:
+        - data: input data
+        - col_name: the column name to be checked
+        - threshold: the threshold for detecting outliers using z-score
+        '''
+
+        data = data if data is not None else self.data
+
+        z_scores = np.abs(stats.zscore(self.data[col_name]))
+        outliers_index_list = np.where(z_scores > threshold)
+        print("%d outliers detected" % len(outliers_index_list[0]))
+
+        # get the lower and upper bound of the outliers
+        # lower_bound = np.mean(data[col_name]) - threshold * np.std(data[col_name])
+        # upper_bound = np.mean(data[col_name]) + threshold * np.std(data[col_name])
+        # print("Lower bound: %.2f" % lower_bound)
+        # print("Upper bound: %.2f" % upper_bound)
+
+        eda_vis = EDA_Visualization()
+        eda_vis.visualize_outliers(data, col_name, outliers_index_list)
+
+        return
--- Data_Visualization/eda.py
+++ Data_Visualization/eda.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 
 class EDA_Visualization:
-    def __init__(self, data):
+    def __init__(self, data: pd.DataFrame = None):
         self.data = data
 
     def visualize_missing_data(self, data: pd.DataFrame = None):
@@ -12,21 +13,19 @@ class EDA_Visualization:
         Parameters:
         - data: the input data
 
-        Returns:
-        - the missing data plots
         """
 
         data = data if data is not None else self.data
     
-        if self.data.shape[1] > 200:
+        if data.shape[1] > 200:
             print("Too many features to visualize")
             return
         num_cols_one_plot = 40
-        num_plots = self.data.shape[1] // num_cols_one_plot + 1
+        num_plots = data.shape[1] // num_cols_one_plot + 1
         for i in range(num_plots):
             start = i * num_cols_one_plot
-            end = min((i+1) * num_cols_one_plot, self.data.shape[1])
-            missing_counts = self.data.iloc[:, start:end].isnull().sum()
+            end = min((i+1) * num_cols_one_plot, data.shape[1])
+            missing_counts = data.iloc[:, start:end].isnull().sum()
             # Create a bar plot of missing values using Plotly
             fig = go.Figure(data=[go.Bar(x=missing_counts.index, y=missing_counts)])
             fig.update_layout(
@@ -40,3 +39,25 @@ class EDA_Visualization:
             fig.show()
             
         return
+    
+    def visualize_outliers(self, data: pd.DataFrame = None, col_name: str = None, outlier_index_list: list = None):
+        """
+        Visualize the outliers
+
+        Parameters:
+        - data: the input data
+        - col_name: the name of the column to be visualized
+        - outlier_index_list: the list of indices of outliers (not timestamps)
+
+        """
+        data = data if data is not None else self.data
+        fig = px.line(data, x=data.index, y=col_name, title='Outliers for %s' % col_name)
+        fig.add_trace(go.Scatter(x=data.index[outlier_index_list], y=data[col_name].values[outlier_index_list], mode='markers', name='outliers'))
+        fig.update_layout(
+            xaxis_title="Date",
+            yaxis_title="Value",
+        )
+        fig.show()
+
+        return
+
