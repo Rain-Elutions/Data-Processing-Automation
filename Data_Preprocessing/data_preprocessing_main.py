83c6ba2713869fc97c6bb8df932c25c4f3659476
Zhuoyu Feng <zfeng0401@elutions.com>
2023-08-11 16:31:39 -0400

    update data preprocessing class

--- Data_Preprocessing/data_preprocessing_main.py
+++ Data_Preprocessing/data_preprocessing_main.py
@@ -4,6 +4,7 @@ from category_encoders import TargetEncoder
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
+
 class FeatureEncoding:
     def __init__(self, data: pd.DataFrame = None, target_name: str = None):
         self.data = data
@@ -24,6 +25,13 @@ class FeatureEncoding:
         return boolean_cols
 
     def get_categorical_columns(self) -> list:
+        '''
+        Get categorical columns from a DataFrame
+
+        Return:
+        - categorical_cols: a list of categorical columns
+        '''
+
         categorical_cols = self.data.select_dtypes(include=['object']).columns.tolist()
         # delete the target column if it is in the list
         if self.target_name in categorical_cols:
@@ -32,6 +40,13 @@ class FeatureEncoding:
         return categorical_cols
     
     def target_encoding(self) -> pd.DataFrame:
+        '''
+        Target encoding for categorical columns
+
+        Return:
+        - data: a DataFrame with categorical columns encoded by target encoding
+        '''
+
         data = self.data
         categorical_cols = self.get_categorical_columns()
 
@@ -103,6 +118,13 @@ class FeatureScaling:
 
     # apply the same scaling or normalization to validation and test sets that applied to the training set
     def get_numerical_columns(self) -> list:
+        '''
+        Get numerical columns from a DataFrame
+
+        Return:
+        - numerical_cols: a list of numerical columns
+        '''
+
         numerical_cols = self.data.select_dtypes(include=['int64', 'float64']).columns.tolist()
         # delete the target column if it is in the list
         if self.target_name in numerical_cols:
@@ -111,6 +133,13 @@ class FeatureScaling:
         return numerical_cols
     
     def min_max_scaling(self) -> pd.DataFrame:
+        '''
+        Min-max scaling for numerical columns
+
+        Return:
+        - data: a DataFrame with numerical columns scaled by min-max scaling
+        '''
+
         data = self.data
         numerical_columns = self.get_numerical_columns()
 
@@ -134,6 +163,13 @@ class FeatureScaling:
         return data
     
     def standard_scaling(self) -> pd.DataFrame:
+        '''
+        Standard scaling for numerical columns
+
+        Return:
+        - data: a DataFrame with numerical columns scaled by standard scaling
+        '''
+
         data = self.data
         numerical_columns = self.get_numerical_columns()
 
@@ -155,23 +191,129 @@ class FeatureScaling:
         data = pd.concat([X_train, X_val, X_test])
 
         return data
-    
 
-class DataPreprocessing:
+
+class FeatureEngineering:
     def __init__(self, data: pd.DataFrame = None):
         self.data = data
 
-    def feature_encoding(self, data: pd.DataFrame) -> pd.DataFrame:
+    def add_time_lag_features(self, data: pd.DataFrame, target_name: str, max_lag: int = 1) -> pd.DataFrame:
+        '''
+        Add time lag features to a DataFrame
+
+        Parameters:
+        - data: the input DataFrame
+        - max_lag: the max time lag to add
+
+        Return:
+        - data: the DataFrame with time lag features added
+        '''
+
         data = data if data is not None else self.data
+
+        for time_lag in range(1, max_lag+1):
+            data['time_lag_' + str(time_lag)] = data[target_name].shift(time_lag)
+
+        return data
+    
+    def add_time_features(self, data: pd.DataFrame) -> pd.DataFrame:
+        '''
+        Add time features to a DataFrame
+
+        Parameters:
+        - data: the input DataFrame
+
+        Return:
+        - data: the DataFrame with time features added
+        '''
+
+        data = data if data is not None else self.data
+
+        data['year'] = data.index.year
+        data['month'] = data.index.month
+        data['day'] = data.index.day
+        data['day_of_week'] = data.index.dayofweek
+        data['day_of_year'] = data.index.dayofyear
+        data['week_of_year'] = data.index.weekofyear
+
+        return data
+    
+
+class DataPreprocessing:
+    def __init__(self, data: pd.DataFrame = None, target_name: str = None):
+        self.data = data
+        self.target_name = target_name
+
+    def feature_encoding(self, data: pd.DataFrame) -> pd.DataFrame:
+        '''
+        Feature encoding for boolean and categorical columns (target encoding by default)
+
+        Parameters:
+        - data: the input DataFrame
+
+        Return:
+        - data: the DataFrame with encoded columns
+        '''
+
+        data = data if data is not None else self.data
+
+        feature_encoding = FeatureEncoding(data, self.target_name)
+        bool_cols = feature_encoding.get_boolean_columns()
+        # categorical_cols = feature_encoding.get_categorical_columns()
+
+        data = feature_encoding.boolean_encoding(bool_cols)
+        data = feature_encoding.target_encoding()
+
+        return data
 
     def feature_scaling(self, data: pd.DataFrame) -> pd.DataFrame:
+        '''
+        Feature scaling for numerical columns (min-max scaling by default)
+
+        Parameters:
+        - data: the input DataFrame
+
+        Return:
+        - data: the DataFrame with scaled numerical columns
+        '''
+
         data = data if data is not None else self.data
 
-    def data_resampling(self, data: pd.DataFrame) -> pd.DataFrame:
+        feature_scaling = FeatureScaling(data, self.target_name)
+        data = feature_scaling.min_max_scaling()
+
+        return data
+
+    def data_resampling(self, data: pd.DataFrame, frequency: str) -> pd.DataFrame:
+        '''
+        Data resampling for time series data
+
+        Parameters:
+        - data: the input DataFrame
+        - frequency: the frequency of resampling (hourly, daily, weekly, monthly)
+
+        Return:
+        - data: the resampled DataFrame
+        '''
+
         data = data if data is not None else self.data
+
+        if frequency == 'hour' or 'Hour' or 'h' or 'H':
+            data = data.resample('H').mean()
+        elif frequency == 'day' or 'Day' or 'd' or 'D':
+            data = data.resample('D').mean()
+        elif frequency == 'week' or 'Week' or 'w' or 'W':
+            data = data.resample('W').mean()
+        elif frequency == 'month' or 'Month' or 'm' or 'M':
+            data = data.resample('M').mean()
+        else:
+            print('Invalid frequency!')
+
+        return data
 
     def data_splitting(self, data: pd.DataFrame = None, target_name: str = '', test_size: float = 0.1, shuffle: bool = False) -> pd.DataFrame:
         data = data if data is not None else self.data
+
         X = data.drop(target_name, axis=1)
         y = data[target_name]
 
@@ -179,9 +321,6 @@ class DataPreprocessing:
         X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, shuffle=shuffle, random_state=42)
 
         return X_train, X_val, X_test, y_train, y_val, y_test
-
-    def feature_engineering(self, data: pd.DataFrame) -> pd.DataFrame:
-        data = data if data is not None else self.data
 
 
 # Test
