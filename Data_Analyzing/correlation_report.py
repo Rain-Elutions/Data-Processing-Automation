f781bc25d471c9320e9d67106d55c4a2e8819797
KyleRodriguez96 <89027392+KyleRodriguez96@users.noreply.github.com>
2023-08-29 13:42:27 -0400

    update to add non linear correlation tests
    still needs testing

--- Data_Analyzing/correlation_report.py
+++ Data_Analyzing/correlation_report.py
@@ -1,5 +1,9 @@
 import pandas as pd 
 import plotly.express as px
+import numpy as np
+from minepy import MINE
+from sklearn.feature_selection import mutual_info_regression
+from scipy.spatial.distance import correlation
 
 class CorrelationReport:
     def __init__(self, data, topn, target_name):
@@ -20,7 +24,72 @@ class CorrelationReport:
          labels_to_drop = self.get_redundant_pairs(self.df)
          au_corr = au_corr.drop(labels=labels_to_drop).sort_values(ascending=False)
          return au_corr
-              
+    
+    def non_linear(self):
+         def calculate_dist_corr(df: pd.DataFrame) -> float:
+            dist_corr_matrix = correlation(df.values.T)
+            dist_corr_df = pd.DataFrame(dist_corr_matrix, columns=df.columns, index=df.columns)
+            return dist_corr_df
+         
+         dist_corr_matrix = calculate_dist_corr(self.df)
+
+         # Export the findings to a CSV file
+         dist_corr_matrix.to_csv('./distance_correlation_matrix.csv', index=True)
+
+         print("Distance correlation matrix:")
+         print(dist_corr_matrix)
+         
+
+         def calculate_MI(df: pd.DataFrame) -> pd.Series:
+            mi_values = []
+            
+            for col1 in df.columns:
+                mi_row = []
+                
+                for col2 in df.columns:
+                    v1 = np.array(df[col1])
+                    v2 = np.array(df[col2])
+                    mi = mutual_info_regression(v1.reshape(-1, 1), v2)[0]
+                    mi_row.append(mi)
+                
+                mi_values.append(mi_row)
+            
+            mi_matrix = pd.DataFrame(mi_values, columns=df.columns, index=df.columns)
+            return mi_matrix
+         df = pd.DataFrame(self.df)
+
+         # Calculate mutual information matrix
+         mi_matrix = calculate_MI(df)
+
+         # Export the mutual information matrix to a CSV file
+         mi_matrix.to_csv('./mutual_information_matrix.csv', index=True)
+
+         print("Mutual Information Matrix:")
+         print(mi_matrix)
+
+         def compute_MIC(v1, v2):
+                mine = MINE(alpha=0.6, c=15, est='mic_approx')
+                mine.compute_score(v1, v2)
+                return round(mine.mic(), 2)
+
+         def calculate_MIC_matrix(df):
+             columns = df.columns
+             mic_matrix = pd.DataFrame(index=columns, columns=columns)
+
+             for col1 in columns:
+                 for col2 in columns:
+                     mic_value = compute_MIC(df[col1], df[col2])
+                     mic_matrix.loc[col1, col2] = mic_value
+
+             return mic_matrix
+         mic_matrix = calculate_MIC_matrix(df)
+
+         # Export the MIC matrix to a CSV file
+         mic_matrix.to_csv('mic_matrix.csv')
+
+         print("MIC Matrix:")
+         print(mic_matrix)
+
     def correlations(self):
             '''
             Function to create correlation csvs
